5d527d287bb1b47a82dcd98d1c518ce07fa2e75f
Candace Johnson <ccj286@nyu.edu>
2018-04-16 13:11:03 +0200

    JS
    automatic age change

--- js/init.js
+++ js/init.js
@@ -3,5 +3,17 @@
 
     $('.button-collapse').sideNav();
 
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var day = d.getDate();
+    var age = year - 1997;
+
+    if (month <= 3) {
+      age = age - 1;
+    }
+
+    $('#age').innerHTML = age;
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
