724686b37ed130b18d611edbd8ea1e83e8feb097
Candace Johnson <ccj286@nyu.edu>
2018-05-13 11:02:41 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -14,7 +14,8 @@
       age = age - 1;
     }
 
-    $('#age').html(age);
+    document.getElementById('age').innerHTML = age;
+    //$('#age').html(age);
     console.log(age);
 
   }); // end of document ready
